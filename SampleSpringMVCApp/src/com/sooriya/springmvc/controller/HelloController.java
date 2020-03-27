0e655128ec9225f4a160d8ed5631f1e81ac22a83
NideeshSurya <nideeshsurya@gmail.com>
2020-03-28 03:04:12 +0530

    T1toT6 | Intro to Spring MVC, setup first app

---
+++ SampleSpringMVCApp/src/com/sooriya/springmvc/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.sooriya.springmvc.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloController extends AbstractController {
+
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ModelAndView modelAndView = new ModelAndView("HelloPage");
+        modelAndView.addObject("message","Hey you... Its working");
+        return modelAndView;
+    }
+}
---
+++ SampleSpringMVCApp/web/jsp/HelloPage.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: nideeshsurya
+  Date: 28/03/20
+  Time: 1:33 am
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Sample Spring MVC App</title>
+</head>
+<body>
+    <h1>Welcome !</h1>
+    <h2>${message}</h2>
+</body>
+</html>
