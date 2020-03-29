ebc02d99b047f32153977a3633142a77fb7e09a7
NideeshSurya <nideeshsurya@gmail.com>
2020-03-29 16:00:38 +0530

    Used @InitBinder annotation and WebDataBinder class, implmented Property Editor class for Date

--- SampleSpringMVCApp/src/com/sooriya/springmvc/controller/StudentAdmissionController.java
+++ SampleSpringMVCApp/src/com/sooriya/springmvc/controller/StudentAdmissionController.java
@@ -1,21 +1,45 @@
 package com.sooriya.springmvc.controller;
 
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Controller
 public class StudentAdmissionController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        /**
+         * this will avoid data binding of mentioned field, passes no data for the field though provided.
+          */
+
+        binder.setDisallowedFields(new String[] {"studentHobby"});
+
+        /**
+         * preparing a date pattern here, which in term allows the same pattern from user other wise throws exception.
+         * There are several Property Editor classes provided by Spring MVC to customize data binding.
+          */
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy**MM**dd");
+        binder.registerCustomEditor(Date.class, "studentDOB", new CustomDateEditor(dateFormat,false));
+    }
 
     @RequestMapping(value = "/admission.html", method = RequestMethod.GET)
     public ModelAndView getAdmissionForm() {
         ModelAndView modelAndView = new ModelAndView("admissionForm");
         return modelAndView;
     }
+
 
     @RequestMapping(value = "/submitForm.html", method = RequestMethod.POST)
     public ModelAndView submitForm(@ModelAttribute("studentObj") Student student, BindingResult bindingResult) {
